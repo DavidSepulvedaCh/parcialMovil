4f9a7ff0df6a0a1cb7461dab3503d9879e2f42ae
Daniel Patiño <daniel.patino.2020@upb.edu.co>
2023-03-03 12:06:56 -0500

    Vista de cuadrícula

--- lib/exports.dart
+++ lib/exports.dart
@@ -1,6 +1,7 @@
 export 'package:flutter/material.dart';
 export 'package:parcial/pages/home.dart';
 export 'package:parcial/models/products.dart';
-export  'package:flutter/services.dart';
+export 'package:flutter/services.dart';
 export 'package:hexcolor/hexcolor.dart';
 export 'package:parcial/pages/login.dart';
+export 'package:parcial/pages/grilla.dart';
---
+++ lib/pages/grilla.dart
@@ -0,0 +1,117 @@
+import 'dart:convert';
+import 'package:flutter/services.dart';
+import 'package:parcial/exports.dart';
+import 'package:http/http.dart' as http;
+
+class Grilla extends StatefulWidget {
+  const Grilla({super.key});
+
+  @override
+  State<Grilla> createState() => _GrillaState();
+
+  static count({required int crossAxisCount, required List<Center> children}) {}
+}
+
+class _GrillaState extends State<Grilla> {
+  List<Product> productss = <Product>[];
+  Future<List<Product>> getProducts() async {
+    var url = 'https://api.npoint.io/9c5fef5b63af7f36fb2d';
+    var rta =
+        await http.get(Uri.parse(url)).timeout(const Duration(seconds: 90));
+    var datos = jsonDecode(rta.body);
+    var register = <Product>[];
+    for (datos in datos) {
+      register.add(Product.fromJson(datos));
+    }
+
+    return register;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getProducts().then((value) {
+      setState(() {
+        productss.addAll(value);
+      });
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.deepOrange,
+          appBar: AppBar(title: const Text('Lista de usuarios')),
+          body: GridView.count(
+            padding: const EdgeInsets.all(10),
+            crossAxisSpacing: 5,
+            mainAxisSpacing: 5,
+            crossAxisCount: 2,
+            children: List.generate(productss.length, (index) {
+              return Card(
+                  margin: const EdgeInsets.all(5),
+                  child: Column(
+                    children: [
+                      Column(
+                        children: const [
+                          /*Image.network(productss[index].image!,
+                              height: 90, fit: BoxFit.cover)*/
+                          SizedBox(
+                            height: 90,
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            productss[index].name!,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Raleway'),
+                          ),
+                          Text(
+                            productss[index].userName!,
+                            textAlign: TextAlign.left,
+                            style: const TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontFamily: 'Raleway'),
+                          ),
+                          Row(
+                            children: [
+                              const SizedBox(
+                                width: 20,
+                              ),
+                              Text(
+                                productss[index].calification!,
+                                style: const TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontFamily: 'Raleway'),
+                              ),
+                              const SizedBox(
+                                width: 85,
+                              ),
+                              IconButton(
+                                icon: const Icon(Icons.star),
+                                onPressed: () {},
+                              )
+                            ],
+                          )
+                        ],
+                      )
+                    ],
+                  ));
+            }),
+          ),
+          bottomNavigationBar: BottomAppBar(
+              shape: const CircularNotchedRectangle(),
+              height: 50,
+              child: IconButton(
+                icon: const Icon(Icons.swap_horizontal_circle_rounded),
+                onPressed: () {},
+              )),
+        ));
+  }
+}
